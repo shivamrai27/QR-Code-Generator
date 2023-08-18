1a4e620ede5985b041b0c374888c63447d926202
Shivam Rai <200shivamrai@gmail.com>
2023-08-18 22:39:58 +0530

    QR

--- script.js
+++ script.js
@@ -2,26 +2,36 @@ const qrFormEl = document.getElementById("qrForm");
 const qrImageEl = document.getElementById("qrImage");
 const qrContainerEl = document.getElementById("qrContainer");
 const qrInputTextEl = document.getElementById("qrInputText");
+const generateBtnEl = document.getElementById("generateBtn");
 
 const renderQRCode = (url) => {
-    if (!url) return
+    if (!url) return;
+    generateBtnEl.innerText = "Generating Qr Code...";
     qrImageEl.src = url;
-    qrContainerEl.classList.add("show");
+
+    const onImageLoad = () => {
+        const interval = setInterval(() => {
+            qrContainerEl.classList.add("show");
+            clearInterval(interval);
+            generateBtnEl.innerText = "Genrate QR Code";
+        }, 500);
+    };
+
+    qrImageEl.addEventListener("load", onImageLoad);
 };
 
 qrFormEl.addEventListener("submit", (event) => {
     event.preventDefault();
 
     const formData = new FormData(qrFormEl);
-    const text = formData.get('qrText');
-    const qrCodeUrl = ` https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${text}`
+    const text = formData.get("qrText");
+    const qrCodeUrl = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${text}`;
+
     renderQRCode(qrCodeUrl);
-    console.log(text);
 });
-
 qrInputTextEl.addEventListener("keyup", () => {
-    if (!qrContainerEl.ariaValueMax.trim());
-    qrContainerEl.classList.remove("showing");
-
+    if (!qrInputTextEl.value.trim()) {
+        qrContainerEl.classList.remove("show");
+    }
 });
 
