cfa1ec168f27542b07907cef6e068c4a4b16792b
Shivam Rai <200shivamrai@gmail.com>
2023-08-18 22:20:28 +0530

    QR

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-const inputEl = document.getElementById('input')
+const qrFormEl = document.getElementById("qrForm");
+const qrImageEl = document.getElementById("qrImage");
+const qrContainerEl = document.getElementById("qrContainer");
+const qrInputTextEl = document.getElementById("qrInputText");
+
+const renderQRCode = (url) => {
+    if (!url) return
+    qrImageEl.src = url;
+    qrContainerEl.classList.add("show");
+};
+
+qrFormEl.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(qrFormEl);
+    const text = formData.get('qrText');
+    const qrCodeUrl = ` https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${text}`
+    renderQRCode(qrCodeUrl);
+    console.log(text);
+});
+
+qrInputTextEl.addEventListener("keyup", () => {
+    if (!qrContainerEl.ariaValueMax.trim());
+    qrContainerEl.classList.remove("showing");
+
+});
 
